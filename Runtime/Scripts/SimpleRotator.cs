e4522022b8a2d402e0b3fee28a65b7423a397875
mauriciosescobar <snkpit@gmail.com>
2026-07-16 18:39:53 -0300

    Modifying the package structure

---
+++ Runtime/Scripts/SimpleRotator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+
+namespace VISLab.Workshop
+{
+    public class SimpleRotator : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeeed;
+
+        void Update()
+        {
+            transform.Rotate(Vector3.up * rotationSpeeed * Time.deltaTime);
+        }
+    }
+}
